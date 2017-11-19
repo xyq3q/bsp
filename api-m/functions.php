3692795c1ce604007c6ce9841648186a6ec5462e
edboffical <edb@paituo.me>
2017-11-19 20:43:09 +0800

    mod api

--- api-m/functions.php
+++ api-m/functions.php
@@ -33,6 +33,20 @@
         Header("Location:success.html"); 
       }
   }
+  function b_post($url, $post_data){
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-type:application/x-www-form-urlencoded',
+            'content' => $postdata,
+            'timeout' => 15 * 60 // 超时时间（单位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+  }
   function ss_set($ip,$port,$pwd,$limit,$dt){
       global $result;
       $bsp_key=$result['bsp_key'];
